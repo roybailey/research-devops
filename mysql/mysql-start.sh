66b969fb4613df0cb09ab6ed811787246b0095ab
Roy Bailey <roybaileybiz@gmail.com>
2018-12-19 13:39:24 +0000

    docker mysql and wordpress update, with docker-compose

--- mysql/mysql-start.sh
+++ mysql/mysql-start.sh
@@ -9,7 +9,7 @@ docker run --name wordpress-mysql -d \
   -e MYSQL_ROOT_PASSWORD=localhost \
   -e MYSQL_DATABASE=wordpress -e MYSQL_USER=wordpress -e MYSQL_PASSWORD=localhost \
   -p 3306:3306 \
-  mysql:latest
+  mysql:5.7
 
 read -p "Press any key to Run the MySQL wait command... " -n1 -s
 
--- wordpress/wordpress-start.sh
+++ wordpress/wordpress-start.sh
@@ -4,7 +4,7 @@
 # for a users-service user which can access it.
 echo "Starting WORDPRESS..."
 
-docker run -e WORDPRESS_DB_PASSWORD=localhost -d \
+docker run -e WORDPRESS_DB_PASSWORD=localhost \
   --name wordpress --link wordpress-mysql:mysql \
   -p 8080:80 \
   -v "$PWD/themes":/var/www/html/wp-content/themes \
